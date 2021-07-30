e9bdc8452d27723011f2230b77751710bc0770d6
Subodh Pandey <spsubodh1999@gmail.com>
2021-07-30 16:21:34 +0530

    Added components and Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+
+export default function Navbar(){
+    return (
+        <header  className="bg-red-600">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink to="/" exact 
+                    activeClassName="text-white"
+                    className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
+                        MyPortfolio
+                    </NavLink>
+                    <NavLink to="/post" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                        Blog Post
+                    </NavLink>
+                    <NavLink  to="/project" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                        Projects
+                    </NavLink>
+                    <NavLink to="/about" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                        About ME!
+                    </NavLink>
+                </nav>
+            </div>
+        </header>
+    )
+}
---
+++ src/components/SinglePost.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Singlepost(){
+    return <h1>SinglePost Page</h1>
+}
